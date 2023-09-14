b35db44b1a7ad79552458ee0dcdb1a5d03cfb084
aprax11 <jashaspringmann@web.de>
2023-09-14 18:33:45 +0200

    refined Tests for ProductProducer

--- src/test/java/com/example/ApiGateway/port/ProductProducerTest.java
+++ src/test/java/com/example/ApiGateway/port/ProductProducerTest.java
@@ -1,5 +1,6 @@
 package com.example.ApiGateway.port;
 
+import com.example.ApiGateway.core.domain.model.MessageType;
 import com.example.ApiGateway.core.domain.model.Product;
 import com.example.ApiGateway.exceptions.ErrorResponseException;
 import com.example.ApiGateway.port.producer.ProductProducer;
@@ -7,6 +8,7 @@ import com.google.gson.Gson;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -14,15 +16,18 @@ import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
+import static com.example.ApiGateway.core.domain.model.MessageType.GET_PRODUCT;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(MockitoExtension.class)
 public class ProductProducerTest {
@@ -30,7 +35,7 @@ public class ProductProducerTest {
     public static final String TEST = "test";
     public static final String TEST_PRODUCT_ONE = "Ring";
     public static final String TEST_PRODUCT_TWO = "Kette";
-    public static final String TEST_USER = "testUser";
+    public static final String TEST_ID = UUID.randomUUID().toString();
     public static final String ROUTING_KEY = "routingKey";
 
     public static final List<Product> ALL_PRODUCTS = List.of(
@@ -88,5 +93,39 @@ public class ProductProducerTest {
 
         assertThat(receivedResponse.get(0).getName()).isEqualTo(TEST_PRODUCT_ONE);
         assertThat(receivedResponse.get(1).getName()).isEqualTo(TEST_PRODUCT_TWO);
+    }
+    @Test
+    void getProductEmptyResponseMessageTest() {
+        when(directExchange.getName()).thenReturn(TEST);
+
+        assertThrows(ErrorResponseException.class, () ->
+                productProducer.sendGetProductMessage(TEST_ID));
+    }
+    @Test
+    void getProductTest() {
+
+        when(directExchange.getName()).thenReturn(TEST);
+        when(rabbitTemplate.sendAndReceive(eq(TEST), eq(ROUTING_KEY), any(Message.class)))
+                .thenReturn(new Message((new Gson().toJson(ALL_PRODUCTS.get(0))).getBytes()));
+
+
+        var receivedResponse = productProducer.sendGetProductMessage(TEST_ID);
+
+        ArgumentCaptor<Message> argumentCaptor = ArgumentCaptor.forClass(Message.class);
+        verify(rabbitTemplate).sendAndReceive(anyString(), anyString(), argumentCaptor.capture());
+
+        Message capturedArgument = argumentCaptor.getValue();
+
+        String capturedArgumentBody = new String(capturedArgument.getBody(), StandardCharsets.UTF_8);
+
+        MessageType capturedArgumentMessageType = MessageType.valueOf(capturedArgument
+                .getMessageProperties()
+                .getType());
+
+        assertThat(capturedArgumentBody).isEqualTo(TEST_ID);
+
+        assertThat(capturedArgumentMessageType).isEqualTo(GET_PRODUCT);
+
+        assertThat(receivedResponse.getName()).isEqualTo(TEST_PRODUCT_ONE);
     }
 }
